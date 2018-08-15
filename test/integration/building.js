d10b1872086dd3ecb305b458264affc0b58f688b
Thomas Prelot <tprelot@gmail.com>
2018-08-15 11:55:56 +0200

    Upgrade integration tests

--- test/integration/building.js
+++ test/integration/building.js
@@ -5,6 +5,7 @@ const nead = require('../..');
 const Container = require('../../src/Container');
 const CyclicDependencyError = require('../../src/errors/CyclicDependency');
 
+// Force injection integration tests execution before.
 require('./injection');
 
 function Foo() {
@@ -94,6 +95,130 @@ describe('nead', () => {
         CyclicDependencyError,
         'Cyclic dependency [\'foo\' < \'bar\' < \'foo\']'
       );
+    });
+
+    it('should handle all default definition factories', () => {
+      const container = nead.createContainer();
+      container.createSet({
+        data: {
+          factory: 'data',
+          data: {
+            plop: {
+              foo: 'foo'
+            },
+            plip: 'plip'
+          },
+          schema: {
+            plop: {
+              type: Object,
+              properties: {
+                foo: {
+                  type: 'string'
+                },
+                bar: {
+                  type: 'string',
+                  default: 'bar'
+                }
+              }
+            },
+            plip: {
+              type: 'string'
+            }
+          }
+        },
+        factory: {
+          factory: 'factory',
+          object: {plip: 'plip'},
+          dependencies: {
+            bar: '#data.plop.bar'
+          }
+        },
+        list: {
+          factory: 'list',
+          items: [
+            '#data.plop.foo',
+            '#data.plop.bar',
+            '#data.plop.foo#data.plop.bar'
+          ]
+        },
+        registry: {
+          factory: 'registry',
+          type: 'element',
+          items: {
+            data: '#data',
+            factory: '#factory',
+            list: '#list',
+            service: '#service',
+            setRegistry: '#setRegistry',
+            setList: '#setList'
+          }
+        },
+        service: {
+          factory: 'service',
+          object: {plap: 'plap'},
+          dependencies: {
+            foo: 'foo',
+            bar: '#data.plop.bar'
+          }
+        },
+        set: {
+          factory: 'set',
+          items: {
+            plop: {
+              object: {plop: 'plop'},
+              dependencies: {
+                foo: '#data.plop.foo'
+              }
+            },
+            plip: {
+              object: {plip: 'plip'},
+              dependencies: {
+                foo: 'plip'
+              }
+            }
+          },
+          dependencies: {
+            bar: '#data.plop.bar'
+          },
+          registry: 'setRegistry',
+          list: 'setList'
+        }
+      }, true);
+
+      expect(container.get('data')).to.deep.equal({
+        plop: {
+          foo: 'foo',
+          bar: 'bar'
+        },
+        plip: 'plip'
+      });
+      const registry = container.get('registry');
+      expect(registry.get('factory').create({plop: 'plop'})).to.deep.equal({
+        plop: 'plop',
+        plip: 'plip',
+        bar: 'bar'
+      });
+      expect(registry.get('list')[2]).to.deep.equal('foobar');
+      expect(registry.get('service')).to.deep.equal({
+        plap: 'plap',
+        foo: 'foo',
+        bar: 'bar'
+      });
+      expect(registry.get('setRegistry').get('plop')).to.deep.equal({
+        plop: 'plop',
+        foo: 'foo',
+        bar: 'bar'
+      });
+      expect(registry.get('setList')[1]).to.deep.equal({
+        plip: 'plip',
+        foo: 'plip',
+        bar: 'bar'
+      });
+      expect(container.get('set.plop')).to.deep.equal({
+        plop: 'plop',
+        foo: 'foo',
+        bar: 'bar'
+      });
     });
   });
 });
